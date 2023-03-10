f1b238277217e9ce6906873ff9920102950e5206
MATHEUS ZANONI BITTENCOURT DE OLIVEIRA <oliveira.4942@aluno.pr.senac.br>
2023-03-10 08:38:58 -0300

    salvar

---
+++ atividade.php
@@ -0,0 +1,88 @@
+ <?php
+    $a = 1;
+    $b = 2;
+    $c = 3;
+    $d = 4;
+    $e = 5;
+    $f = 6;
+    $g = 7;
+    $h = 8;
+    $i = 9;
+    $j = 10;
+
+    echo "Tabuada do 1";
+    echo("</br>");
+    while ($a <= 10){
+        echo " Número = " . $a . "</br>";
+        $a = $a +1;
+    }
+    echo("</br>");
+    echo "Tabuada do 2";
+    echo("</br>");
+    while ($b <= 20){
+        echo " Número = " . $b . "</br>";
+        $b = $b +2;
+    }
+    echo("</br>");
+    echo "Tabuada do 3";
+    echo("</br>");
+    while ($c <= 30){
+        echo " Número = " . $c . "</br>";
+        $c = $c +3;
+    }
+    echo("</br>");
+    echo "Tabuada do 4";
+    echo("</br>");
+    while ($d <= 40){
+        echo " Número = " . $d . "</br>";
+        $d = $d +4;
+    }
+
+    echo("</br>");
+    echo "Tabuada do 5";
+    echo("</br>");
+    while ($e <= 50){
+        echo " Número = " . $e . "</br>";
+        $e = $e +5;
+    }
+
+    echo("</br>");
+    echo "Tabuada do 6";
+    echo("</br>");
+    while ($f <= 60){
+        echo " Número = " . $f . "</br>";
+        $f = $f +6;
+    }
+
+    echo("</br>");
+    echo "Tabuada do 7";
+    echo("</br>");
+    while ($g <= 70){
+        echo " Número = " . $g . "</br>";
+        $g = $g +7;
+    }
+
+    echo("</br>");
+    echo "Tabuada do 8";
+    echo("</br>");
+    while ($h <= 80){
+        echo " Número = " . $h . "</br>";
+        $h = $h +8;
+    }
+
+    echo("</br>");
+    echo "Tabuada do 9";
+    echo("</br>");
+    while ($i <= 90){
+        echo " Número = " . $i . "</br>";
+        $i = $i +9;
+    }
+
+    echo("</br>");
+    echo "Tabuada do 10";
+    echo("</br>");
+    while ($j <= 100){
+        echo " Número = " . $j . "</br>";
+        $j = $j +10;
+    }
+    ?>
