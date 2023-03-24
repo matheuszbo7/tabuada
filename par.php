c11217bfd58dcbd6a132d68169eec0f519b81079
MATHEUS ZANONI BITTENCOURT DE OLIVEIRA <oliveira.4942@aluno.pr.senac.br>
2023-03-24 08:25:59 -0300

    salvar

--- par.php
+++ par.php
@@ -1,5 +1,10 @@
-  <h1>Números Pares de 0 a 500</h1>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+ <h1>Números Pares de 0 a 500</h1>
+
     <?php
+        include("menu.php");
+
      for ($par=0; $par<=500; $par++){
         if (($par % 2 == 1) and ($par < 500)){
             continue;
@@ -11,3 +16,4 @@
         }
         }
     ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
